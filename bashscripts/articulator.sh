90ecf1a90b8787d8990905390ea84467392c6430
sambein <samuel.bein@gmail.com>
2019-01-20 23:38:30 -0600

    added best response functions

--- bashscripts/articulator.sh
+++ bashscripts/articulator.sh
@@ -1,7 +1,8 @@
 python tools/articulateSplines.py ResponseFunctionsMC17.root "output/Fall17MiniAODv2.QCD_H/TemplatesRunIIFall17MiniAODv2.QCD_*JerNom*.root"
 python tools/articulateSplines.py ResponseFunctionsMC17_JerUp.root "output/Fall17MiniAODv2.QCD_H/TemplatesRunIIFall17MiniAODv2.QCD_*JerUp*.root"
-python tools/articulateSplines.py ResponseFunctionsMC16.root "output/Summer16.QCD_HT/TemplatesSummer16.QCD_HT*JerNom*.root"
-python tools/articulateSplines.py ResponseFunctionsMC17_JerUp.root "output/Summer16.QCD_HT/TemplatesSummer16.QCD_HT*JerUp*.root"
+python tools/articulateSplines.py ResponseFunctionsMC16.root "output/Summer16MiniAODv3.QCD_H/TemplatesRunIISummer16MiniAODv3.QCD*JerNom*.root"
+python tools/articulateSplines.py ResponseFunctionsMC16_JerUp.root "output/Summer16MiniAODv3.QCD_H/TemplatesRunIISummer16MiniAODv3.QCD*JerUp*.root"
+
 
 
 '''
