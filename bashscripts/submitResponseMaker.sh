a96b2abbd1cb82559013d6105d213de11844cc5e
sambein <samuel.bein@gmail.com>
2019-01-24 01:47:56 -0600

    some unkown change to submission bash script

--- bashscripts/submitResponseMaker.sh
+++ bashscripts/submitResponseMaker.sh
@@ -1,7 +1,21 @@
-python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Nom
-python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H --JerUpDown Nom
+python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Nom &
+python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Up &
+python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H --JerUpDown Nom &
+python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H  --JerUpDown Up &
 
-python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Up
-python tools/submitjobs.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H  --JerUpDown Up
 
+
+
+
+
+
+
+
+
+
+
+python tools/submitjobsCmsconnect.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Nom &
+python tools/submitjobsCmsconnect.py --analyzer tools/ResponseMaker.py --fnamekeyword Fall17MiniAODv2.QCD_H --JerUpDown Up &
+python tools/submitjobsCmsconnect.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H --JerUpDown Nom &
+python tools/submitjobsCmsconnect.py --analyzer tools/ResponseMaker.py --fnamekeyword Summer16MiniAODv3.QCD_H  --JerUpDown Up &
 
