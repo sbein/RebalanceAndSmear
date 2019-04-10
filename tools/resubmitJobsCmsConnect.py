89141ef5837d5bb5ff725d54787b965ce51864bb
sambein <samuel.bein@gmail.com>
2019-04-10 05:16:25 -0500

    saved submit for editing

--- tools/resubmitJobsCmsConnect.py
+++ tools/resubmitJobsCmsConnect.py
@@ -92,10 +92,12 @@ def main():
                 os.system('mkdir output/'+fnamekeyword.replace(' ',''))
         if resub:
             errname = 'jobs/'+job+'.err'
+            print 'errname', errname
             if not os.path.exists(errname): continue
             errfile = open(errname)
             lines = errfile.readlines()
             errfile.close()
+            print 'lines', lines
             if not resubmessage in lines: continue
         outdir = 'output/'+fnamekeyword.replace(' ','')
         print 'cd '+ outdir
